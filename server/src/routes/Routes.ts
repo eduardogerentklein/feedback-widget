2d421b0883a1a8806c2bbf18d3144e8fe326d39a
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2022-05-04 14:47:58 -0300

    feat: adding server routes

---
+++ server/src/routes/Routes.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+
+import { NodemailerAdapter } from '../adapters/nodemailer/NodeMailerAdapter';
+import { PrismaFeedbacksRepository } from '../repositories/prisma/PrismaFeedbacksRepository';
+import { SubmitFeedbackService } from '../services/SubmitFeedbackService';
+
+export const routes = express.Router()
+
+routes.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+  const nodemailerAdapter = new NodemailerAdapter()
+
+  const submitFeedbackService = new SubmitFeedbackService(
+    prismaFeedbacksRepository, 
+    nodemailerAdapter
+  )
+
+  await submitFeedbackService.execute({
+    type,
+    comment,
+    screenshot
+  })
+
+  return res.status(201).send()
+})
