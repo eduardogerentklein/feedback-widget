7cdec9b62fc187feb0252416ec7d6fd895cd2d35
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2022-05-04 14:48:44 -0300

    feat: adding prisma and Response type

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,12 @@
 import express from 'express'
-import { prisma } from './prisma'
-import { Response } from './types/response'
+import { routes } from './routes'
 
 const app = express()
 
 const PORT = process.env.SERVER_PORT || 3333
 
 app.use(express.json())
-
-app.post('/feedbacks', async (req, res) => {
-  const { type, comment, screenshot } = req.body
-
-  const feedback = await prisma.feedback.create({
-    data: { 
-      type, 
-      comment, 
-      screenshot 
-    }
-  })
-  return res.status(201).json({ data: feedback } as Response)
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`HTTP Server running on port: ${PORT}`)
