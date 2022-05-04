7cbf3b2ff9c457a7b84a2c05ad3c4830aebef8e4
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2022-05-04 14:47:14 -0300

    feat: creating mail adapters

---
+++ server/src/adapters/nodemailer/NodemailerAdapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer'
+import { MailAdapter, SendMailData } from "../MailAdapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "35a3e800bce9c3",
+    pass: "25b319611f6afa"
+  }
+});
+
+export class NodemailerAdapter implements MailAdapter {
+  sendmail = async ({ subject, body }: SendMailData) => {
+    await transport.sendMail({
+      from: 'Eduardo Feedback Widget <oi@feedget.com>',
+      to: 'Eduardo Klein <eduardo.klein@catolicasc.org.br>',
+      subject: subject,
+      html: body
+    })
+  }
+}
