02ae11ecbda264cd4cc20e9d2f15d272b17a548c
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2022-05-06 11:27:33 -0300

    fix: update server port variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,12 @@ import { routes } from './routes'
 
 const app = express()
 
-const PORT = process.env.SERVER_PORT || 3333
+const SERVER_PORT = process.env.PORT || 3333
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(PORT, () => {
-  console.log(`HTTP Server running on port: ${PORT}`)
+app.listen(SERVER_PORT, () => {
+  console.log(`HTTP Server running on port: ${SERVER_PORT}`)
 })
